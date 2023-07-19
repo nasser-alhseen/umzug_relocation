71cc0284d2603e822c68045dd12a488b0bed7f15
Nasser ALhseen <abdalnasralhseen2019@gmail.com>
2023-07-19 22:24:30 +0300

    scrool

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ export default function App() {
   useEffect(() => {
     const sr = scrollreveal({
       origin: "top",
-      distance: "10px",
-      duration: 700,
+      distance: "30px",
+      duration: 900,
       reset: true,
     });
     sr.reveal(
